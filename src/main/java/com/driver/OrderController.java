55ba79ab083377261cef7a6c1f7ad32df68b2c45
Areeff <p.areeff786@gmail.com>
2023-05-30 17:22:26 +0530

    Assignment done

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -18,8 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("orders")
 public class OrderController {
-     @Autowired
-     private OrderService orderService;
+    // @Autowired
+     private OrderService orderService=new OrderService();
     @PostMapping("/add-order")
     public ResponseEntity<String> addOrder(@RequestBody Order order){
         orderService.addOrder(order);
--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -10,8 +10,8 @@ import java.util.Optional;
 
 @Service
 public class OrderService {
-    @Autowired
-    private OrderRepository orderRepository;
+    //@Autowired
+    private OrderRepository orderRepository=new OrderRepository();
 
     public void addOrder(Order order) {
         orderRepository.addOrder(order);
